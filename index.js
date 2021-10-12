ec2770a1c28144e988d745a63cc96aa2bd522bb0
Conjeee <allencabinta@gmail.com>
2021-10-12 17:37:01 +0800

    main package commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let money = 0;
+let moneyEl = document.getElementById("money-el");
+
+function play() {
+    if(money <= 0) {
+        moneyEl.textContent() += 100;
+    }
+}
+
+function quit() {
+
+}
