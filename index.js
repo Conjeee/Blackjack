8af09941393e6f2b341f272182d0b63c99ebd8db
Conjeee <allencabinta@gmail.com>
2021-10-12 22:11:48 +0800

    Added play function

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-
-let money = 0;
-let moneyEl = document.getElementById("money-el");
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let textEl = document.getElementById("text-el")
 
 function play() {
-    if(money <= 0) {
-        moneyEl.textContent() += 100;
+    let cardOne = Math.round(Math.random() * 11)
+    let cardTwo = Math.round(Math.random() * 11)
+
+    console.log(cardOne)
+    let sum = cardOne + cardTwo
+
+    cardsEl.innerText = "Cards: " + cardOne + " and " + cardTwo
+    sumEl.innerText = "Sum: " + sum
+
+    if(sum < 21) {
+        textEl.innerText = "Draw another card?"
+    }
+
+    else if(sum === 21) {
+        textEl.innerText = "You busted!"
+    }
+
+    else {
+        textEl.innerText = "You Won!"
     }
 }
 
