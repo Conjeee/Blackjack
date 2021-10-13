947a3997cc86de79b406b7ff493e957f9698a8ec
Conjeee <allencabinta@gmail.com>
2021-10-14 01:55:03 +0800

    Added Logic to game

--- index.js
+++ index.js
@@ -1,30 +1,81 @@
-let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let textEl = document.getElementById("text-el")
+let drawEl = document.getElementById("draw-el")
+let playEl = document.getElementById("play-el")
+let stayEl = document.getElementById("stay-el")
+let dealerEl = document.getElementById("dealer-el")
+
+let cardOne = 0
+let cardTwo = 0
+let cardThree = 0
+let sum = cardOne + cardTwo + cardThree
+let dealerSum = 0
+let lost = false
 
 function play() {
-    let cardOne = Math.round(Math.random() * 11)
-    let cardTwo = Math.round(Math.random() * 11)
+    cardOne = Math.ceil(Math.random() * 11)
+    cardTwo = Math.ceil(Math.random() * 11)
 
-    console.log(cardOne)
-    let sum = cardOne + cardTwo
+    playEl.style.display = "none"
+    cardsEl.innerText = "Your Cards: " + cardOne + " - " + cardTwo
+    dealerEl.innerText = "Dealer's Cards: "
 
-    cardsEl.innerText = "Cards: " + cardOne + " and " + cardTwo
-    sumEl.innerText = "Sum: " + sum
-
-    if(sum < 21) {
+    if(sum <= 21) {
         textEl.innerText = "Draw another card?"
-    }
-
-    else if(sum === 21) {
-        textEl.innerText = "You busted!"
+        drawEl.style.display = "inline"
+        stayEl.style.display = "inline"
     }
 
     else {
-        textEl.innerText = "You Won!"
+        textEl.innerText = "You busted! Play Again?"
+        lost = true
+        evaluate()
     }
 }
 
-function quit() {
+function draw() {
+    drawEl.style.display = "none"
+    stayEl.style.display = "none"
+    playEl.style.display = "inline"
 
+    cardsEl.innerText += " - " + Math.ceil(Math.random() * 11)
+
+    if(sum > 21) {
+        textEl.innerText = "You Busted! You Lost This Round."
+        lost = true
+    }
+    evaluate()
+}
+
+function stay() {
+    drawEl.style.display = "none"
+    stayEl.style.display = "none"
+    playEl.style.display = "inline"
+    evaluate()
+}
+
+function evaluate() {
+    while(dealerSum < 16) {
+        let num = Math.ceil(Math.random() * 11)
+        dealerSum += num
+        dealerEl.innerText += " - " + num
+    }
+
+    if(lost || (dealerSum <= 21 && dealerSum > sum)) {
+        textEl.innerText = "You Lost! Play Again?"
+    }
+
+    else if(dealerSum > 21) {
+        textEl.innerText = "You Won! Play Again?"
+    }
+
+    else {
+        textEl.innerText = "You Tied! Play Again?"
+    }
+    setNeutral()
+}
+
+function setNeutral() {
+    dealerSum = 0
+    lost = false
 }
