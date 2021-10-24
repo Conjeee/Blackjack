742da94e7e3e24eca058d691c05b785e43044451
Conjiii <allencabinta@gmail.com>
2021-10-24 23:33:31 +0800

    Added better card images

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ let playEl = document.getElementById("play-el")
 let stayEl = document.getElementById("stay-el")
 let dealerEl = document.getElementById("dealer-el")
 
-const tens = ["cardPics/10.jpg","cardPics/J.jpg","cardPics/Q.jpg","cardPics/K.jpg"]
-const cards = ["cardPics/2.jpg","cardPics/3.jpg","cardPics/4.jpg","cardPics/5.jpg","cardPics/6.jpg",
-"cardPics/7.jpg","cardPics/8.jpg","cardPics/9.jpg",randTen(),"cardPics/11.jpg"]
+const cardPictureArray = [
+    ["cardPics/2bc.png","cardPics/2bs.png","cardPics/2rd.png","cardPics/2rh.png"],
+    ["cardPics/3bc.png","cardPics/3bs.png","cardPics/3rd.png","cardPics/3rh.png"],
+    ["cardPics/4bc.png","cardPics/4bs.png","cardPics/4rd.png","cardPics/4rh.png"],
+    ["cardPics/5bc.png","cardPics/5bs.png","cardPics/5rd.png","cardPics/5rh.png"],
+    ["cardPics/6bc.png","cardPics/6bs.png","cardPics/6rd.png","cardPics/6rh.png"],
+    ["cardPics/7bc.png","cardPics/7bs.png","cardPics/7rd.png","cardPics/7rh.png"],
+    ["cardPics/8bc.png","cardPics/8bs.png","cardPics/8rd.png","cardPics/8rh.png"],
+    ["cardPics/9bc.png","cardPics/9bs.png","cardPics/9rd.png","cardPics/9rh.png"],
+    ["cardPics/10bc.png","cardPics/10bs.png","cardPics/10rd.png","cardPics/10rh.png"
+    ,"cardPics/Jbc.png","cardPics/Jbs.png","cardPics/Jrd.png","cardPics/Jrh.png"
+    ,"cardPics/Qbc.png","cardPics/Qbs.png","cardPics/Qrd.png","cardPics/Qrh.png"
+    ,"cardPics/Kbc.png","cardPics/Kbs.png","cardPics/Krd.png","cardPics/Krh.png"],
+    ["cardPics/11bc.png","cardPics/11bs.png","cardPics/11rd.png","cardPics/11rh.png"],
+]
 let sum = 0
 let dealerSum = 0
 let lost = false
@@ -44,7 +56,7 @@ function draw() {
 
     if(sum > 21) {
         lost = true
-        drawEl.style.display = "none"
+        drawEl.style.display = "none"   
         stayEl.style.display = "none"
         playEl.style.display = "inline"
         evaluate()
@@ -81,13 +93,13 @@ function evaluate() {
     lost = false
 }
 
-function randTen() {
-    let num = Math.floor(Math.random() * 4)
-    return tens[num]
+function cardRepresent(num) {
+    let randomCard = Math.ceil(cardPictureArray[num-2].length * Math.random())-1
+    return cardPictureArray[num-2][randomCard]
 }
 
-function addImage(num, slot) {
+function addImage(num, location) {
     let image = document.createElement("img")
-    image.src = cards[num-2]
-    document.getElementById(slot).appendChild(image)
+    image.src = cardRepresent(num)
+    document.getElementById(location).appendChild(image)
 }
