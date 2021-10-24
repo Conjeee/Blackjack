bd8655e44ff2dfbb8006501ca2a06aa7567be7e5
Conjiii <allencabinta@gmail.com>
2021-10-24 14:56:29 +0800

    Added images to game

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ let playEl = document.getElementById("play-el")
 let stayEl = document.getElementById("stay-el")
 let dealerEl = document.getElementById("dealer-el")
 
-const cards = [0,0,0,0,0,0,0,0,0,0,0]
-let cardCount = 2
+const tens = ["cardPics/10.jpg","cardPics/J.jpg","cardPics/Q.jpg","cardPics/K.jpg"]
+const cards = ["cardPics/2.jpg","cardPics/3.jpg","cardPics/4.jpg","cardPics/5.jpg","cardPics/6.jpg",
+"cardPics/7.jpg","cardPics/8.jpg","cardPics/9.jpg",randTen(),"cardPics/11.jpg"]
 let sum = 0
 let dealerSum = 0
 let lost = false
 
 function play() {
-    cards[0] = Math.ceil(Math.random() * 10)+1
-    cards[1] = Math.ceil(Math.random() * 10)+1
-    sum = cards[0] + cards[1]
+    let num1 = Math.ceil(Math.random() * 10)+1
+    let num2 = Math.ceil(Math.random() * 10)+1
+    sum = num1 + num2
 
     playEl.style.display = "none"
-    cardsEl.innerText = "Your Cards: " + cards[0] + " - " + cards[1]
+    cardsEl.innerText = "Your Cards: "
     dealerEl.innerText = "Dealer's Cards: "
+
+    addImage(num1, 'cards-el')
+    addImage(num2, 'cards-el')
 
     if(sum <= 21) {
         textEl.innerText = "Draw another card?"
@@ -34,9 +38,9 @@ function play() {
 }
 
 function draw() {
-    cards[cardCount] = Math.ceil(Math.random() * 10)+1
-    cardsEl.innerText += " - " + cards[cardCount]
-    sum += cards[cardCount++]
+    let num = Math.ceil(Math.random() * 10)+1
+    addImage(num, 'cards-el')
+    sum += num
 
     if(sum > 21) {
         lost = true
@@ -58,7 +62,7 @@ function evaluate() {
     while(dealerSum < 16) {
         let num = Math.ceil(Math.random() * 10)+1
         dealerSum += num
-        dealerEl.innerText += " - " + num
+        addImage(num, 'dealer-el')
     }
 
     if(lost || (dealerSum <= 21 && dealerSum > sum)) {
@@ -75,5 +79,15 @@ function evaluate() {
 
     dealerSum = 0
     lost = false
-    cards.fill(0)
+}
+
+function randTen() {
+    let num = Math.floor(Math.random() * 4)
+    return tens[num]
+}
+
+function addImage(num, slot) {
+    let image = document.createElement("img")
+    image.src = cards[num-2]
+    document.getElementById(slot).appendChild(image)
 }
